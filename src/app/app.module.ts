ff670e112783c5d25817981c995a4a1ba32afc29
Bob Fornal <bob.fornal@leadingedje.com>
2025-04-25 13:02:44 -0400

    Minor Build Adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
+
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/services/cloudflare-api.service.ts
+++ src/app/core/services/cloudflare-api.service.ts
@@ -4,6 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CloudflareApiService {
+  private readonly api_key: string = process.env['AUTH_KEY'] || '';
+  private readonly api_path: string = 'https://database-tool.bob-fornal.workers.dev/api';
 
-  constructor() { }
+  public async getUsersRegisteredToday() {
+    const result = await fetch(`${this.api_path}/get-users-registered-today`, {
+      method: 'GET',
+      headers: {
+        'x-api-key': this.api_key,
+      },
+    });
+  }
+
+  public async postUserData(username: string, email: string) {
+    const result = await fetch(`${this.api_path}/post-user-data`, {
+      method: 'POST',
+      headers: {
+        'x-api-key': this.api_key,
+      },
+      body: JSON.stringify({ username, email }),
+    });
+  }
 }
